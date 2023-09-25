4ebd27b051c6f02563c23bb9ac2577b09c9f3087
pranayjoshi <pranayjoshi4466@gmail.com>
2023-09-25 15:07:18 -0500

    added

--- lab/4/caps.c
+++ lab/4/caps.c
@@ -1,23 +1,39 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 int main()
 {
     int totalW;
+    printf("Enter how many words:\n");
     scanf("%d", &totalW);
-    char out[100];
-    for (int i = 0; i < totalW; i++) {
+    char out[100] = "";
+    printf("Enter %d words:\n", totalW);
+    int res = 0;
+    for (int i = 0; i < totalW; i++)
+    {
         char word[51];
         scanf(" %s", word);
-        for (int i = 0; word[i] != '\0'; i++)
+        int len = 0;
+        while (word[len] != '\0')
         {
-            if (isupper(word[i])){
-                out+=word[i];
+            len++;
+        }
+
+        if (len > 3)
+        {
+            for (int j = 0; j < len; j++)
+            {
+                if (isupper(word[j]))
+                {
+                    out[res] = word[j];
+                    res++;
+                }
             }
         }
     }
 
-    printf("The result is '%s'", out);
+    printf("The result is '%s'\n", out);
 
     return 0;
 }
