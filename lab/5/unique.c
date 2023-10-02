19d69b5e435b7848f37b6e7f645535ad71da3e1c
pranayjoshi <pranayjoshi4466@gmail.com>
2023-10-02 14:41:02 -0500

    added

---
+++ lab/5/unique.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Enter a input:\n");
+    int strA[26] = {0};
+    char str[51];
+    printf("Enter a string: ");
+    fgets(str, sizeof(input), stdin);
+    str[strcspn(str, "\n")] = '\0';
+    for (int i = 0; str[i] != '\0', i++)
+    {
+        int sj = str[i] - 'a';
+        strA[sj]++;
+    }
+    int t = 0;
+    printf("Unique characters include: ")
+    for (int i = 0; i < 26; i++)
+    {
+        if (strA[i] == 1)
+        {
+            t = 1;
+            break;
+        }
+    }
+
+    if (t)
+    {
+        for (int i = 0; i < 26; i++)
+        {
+            if (strA[i] == 1)
+            {
+                printf("%c ", 'a' + i);
+            }
+        }
+    }
+    else{
+        printf("NONE");
+    }
+
+    return 0;
+}
