4d9a27ec08de8531ae037626bd0f080d3f5bae40
pranayjoshi <pranayjoshi4466@gmail.com>
2023-11-13 15:05:07 -0600

    added prac3

--- lab/14/3.c
+++ lab/14/3.c
@@ -3,7 +3,6 @@
 #include <ctype.h>
 #include <stdlib.h>
 
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -13,7 +12,7 @@
 lab3.c
  We again define heft of a string as the sum of its cardinal letter values.
  a|A == 1, b|B == 2, ... z|Z == 26
- 
+
  The command line will specify a filename. Read all words from the file
  Print the last 10 words (or less, if there are not ten) in the file that are greater than average heft. The words must be printed in the order they first appear in the file. Duplicates should be excluded.
 
@@ -27,31 +26,38 @@ lab3.c
  Dog Crimson Clown Crimson Alabama Clown
  Crimson and Clown were greater than average heft but they appeared more than once in the file.
  */
-int card(char ch){
-   
-    //The cardinality of a letter. Case insensitive.
-    if (ch < 'a'){
+int card(char ch)
+{
+
+    // The cardinality of a letter. Case insensitive.
+    if (ch < 'a')
+    {
         return ch - 'A' + 1;
     }
-    else {
+    else
+    {
         return ch - 'a' + 1;
     }
-   return 0;
+    return 0;
 }
-int heft(char * str){
-    //Sum of the letters cardinality. Case insensitive, only letters.
+int heft(char *str)
+{
+    // Sum of the letters cardinality. Case insensitive, only letters.
     int sum = 0;
-    for (int i = 0; i < strlen(str); i++){
+    for (int i = 0; i < strlen(str); i++)
+    {
         sum += card(str[i]);
     }
     return sum;
 }
 
-int main(int argc, char ** argv){
-   //give the heft of command line arguments
-   //eg: "heft" has heft of: 39
-    FILE * inp = fopen(argv[1], "r");
-    if (inp == NULL){
+int main(int argc, char **argv)
+{
+    // give the heft of command line arguments
+    // eg: "heft" has heft of: 39
+    FILE *inp = fopen(argv[1], "r");
+    if (inp == NULL)
+    {
         printf("unable to open %s\n", argv[1]);
         return 0;
     }
@@ -59,45 +65,68 @@ int main(int argc, char ** argv){
     int count = 0;
     int total = 0;
     char words[100][100];
-    while (fscanf(inp, "%s", word) == 1){
+    while (fscanf(inp, "%s", word) == 1)
+    {
         int s = 0;
-        for (int j = 0; j < count; j++){
-            if (strcmp(words[j], word) == 0){
-                s=1;
+        for (int j = 0; j < count; j++)
+        {
+            if (strcmp(words[j], word) == 0)
+            {
+                s = 1;
                 break;
             }
         }
-        if (s == 1){
+        if (s == 1)
+        {
             continue;
         }
         strcpy(words[count], word);
         total += heft(word);
         count++;
     }
-    int avg = total/count;
+    int avg = total / count;
     printf("Average heft: %d\n", avg);
     rewind(inp);
     int last10[10][100];
     int last10count = 0;
-    while (fscanf(inp, "%s", word) == 1 && last10count < 10){
+    while (fscanf(inp, "%s", word) == 1)
+    {
         int s = 0;
-        for (int j = 0; j < last10count; j++){
-            if (strcmp(last10[j], word) == 0){
-                s=1;
+        for (int j = 0; j < last10count; j++)
+        {
+            if (strcmp(last10[j], word) == 0)
+            {
+                s = 1;
                 break;
             }
         }
-        if (s == 1){
+        if (s == 1)
+        {
             continue;
         }
-        if (heft(word) > avg){
+        if (heft(word) > avg)
+        {
             strcpy(last10[last10count], word);
             last10count++;
         }
     }
-    printf("Last %d words over heft of: %d\n", last10count, avg);
-    for (int i = 0; i < last10count; i++){
-        printf("%d. \"%s\" has heft of: %d\n", i+1, last10[i], heft(last10[i]));
+    if (last10count > 10)
+    {
+        printf("Last 10 words over heft of: %d\n", avg);
+        int j = 1;
+        for (int i = last10count-10; i < last10count; i++)
+        {
+            printf("%d. \"%s\" has heft of: %d\n", j, last10[i], heft(last10[i]));
+            j++;
+        }
+    }
+    else
+    {
+        printf("Last %d words over heft of: %d\n", last10count, avg);
+        for (int i = 0; i < last10count; i++)
+        {
+            printf("%d. \"%s\" has heft of: %d\n", i + 1, last10[i], heft(last10[i]));
+        }
     }
 
     return 0;
