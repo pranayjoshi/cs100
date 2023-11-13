535c6559bc2d54b416e503e0b168fa734659e2c9
pranayjoshi <pranayjoshi4466@gmail.com>
2023-11-13 14:13:06 -0600

    added prac3

--- project/5/ItemToPurchase.h
+++ project/5/ItemToPurchase.h
@@ -1,3 +1,6 @@
+#ifndef ITEMTOPURCHASE_H
+#define ITEMTOPURCHASE_H
+
 typedef struct ItemToPurchase_struct {
     char itemName[100];
     char itemDescription[1000];
@@ -8,3 +11,6 @@ typedef struct ItemToPurchase_struct {
 void MakeItemBlank(ItemToPurchase* item);
 void PrintItemCost(ItemToPurchase* item);
 void PrintItemDescription(ItemToPurchase* item);
+
+
+#endif
